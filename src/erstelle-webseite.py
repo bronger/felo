6e85b64e45a16cd05177263f0953894bc73c2717
Torsten Bronger <bronger@physik.rwth-aachen.de>
2006-11-29 18:17:01 +0000

    Optisch etwas ansprechender gemacht.

--- src/erstelle-webseite.py
+++ src/erstelle-webseite.py
@@ -7,20 +7,27 @@ __version__ = "$Revision:  $"
 import codecs, os
 from felo_rating import parse_felo_file, calculate_felo_ratings, write_felo_file
 from subprocess import call
-import shutil
+import shutil, time
 
 os.chdir("../aachen")
 
 def datei_zeitstempel(pfade):
-    import time
     timestamp = 0
     for pfad in pfade:
         timestamp = max(timestamp, os.stat(pfad).st_mtime)
-    return time.strftime(u"%d.&nbsp;%B&nbsp;%Y, %H.%M&nbsp;Uhr", time.localtime(timestamp))
+    return time.strftime(u"%d.&nbsp;%B&nbsp;%Y", time.localtime(timestamp))
+
+def convert_date(date):
+    return time.strftime(u"%d.&nbsp;%B&nbsp;%Y", time.strptime(date, "%Y/%m/%d"))
 
 mache_plots = True
 ausgabedatei = codecs.open("/home/bronger/aktuell/fechten/Intern/felo-zahlen.html",
                            "w", "iso-8859-1")
+foil_parameters, foil_fencers, foil_bouts = parse_felo_file("florett.felo")
+epee_parameters, epee_fencers, epee_bouts = parse_felo_file("degen.felo")
+foil_bouts.sort()
+epee_bouts.sort()
+
 print>>ausgabedatei, u"""<html>
 <head>
 <title>Felo-Zahlen</title>
@@ -32,18 +39,20 @@ print>>ausgabedatei, u"""<html>
 <div align="center">
 <h1>Felo-Zahlen</h1>
 
-<p style="font-size: medium">vom %s</p>
-
 <p style="font-size: large">Die <a href="felo-faq.html">Felo-FAQ</a> &ndash;
                             Fragen und Antworten</p>
 
-<h2 id="florett" style="margin-top:5ex">Florett</h2>
+<table cellspacing="50px" style="background-color: #00a0FF">
+<tbody>
+<tr style="vertical-align:top">
+<td style="text-align:center">
+<h2 id="florett">Florett</h2>
+<p style="font-size: medium;margin-top:-1ex">vom %s</p>
 <p style="font-size:large"><a href="felo-plots.html">Zur Grafik &rarr;</a></p>
-<table cellspacing="10" style="font-size: large">
-<tbody>""" % datei_zeitstempel(("florett.felo", "degen.felo"))
-parameters, fencers, bouts = parse_felo_file("florett.felo")
-# write_felo_file("florett_.felo", parameters, fencers, bouts)
-for fechter in calculate_felo_ratings(parameters, fencers, bouts, plot=mache_plots):
+<p><table cellspacing="10" style="font-size: large; width:100%%">
+<tbody>""" % convert_date(foil_bouts[-1].date[:10])
+# write_felo_file("florett_.felo", foil_parameters, foil_fencers, foil_bouts)
+for fechter in calculate_felo_ratings(foil_parameters, foil_fencers, foil_bouts, plot=mache_plots):
     felo_zahl = str(fechter.felo_rating)
     if fechter.fenced_points < 50:
         felo_zahl = "&asymp;&nbsp;" + felo_zahl
@@ -51,22 +60,26 @@ for fechter in calculate_felo_ratings(parameters, fencers, bouts, plot=mache_plo
 shutil.copy2("florett.png", "/home/bronger/aktuell/fechten/Intern/")
 shutil.copy2("florett.pdf", "/home/bronger/aktuell/fechten/Intern/")
 print>>ausgabedatei, u"""</tbody>
-</table>
-
-<!--h2 id="degen" style="margin-top:4ex">Degen</h2>
+</table></p>
+</td><!--td style="text-align:center">
+<h2 id="degen">Degen</h2>
+<p style="font-size: medium;margin-top:-1ex">vom %s</p>
 <p style="font-size:large"><a href="felo-plots.html#degen">Zur Grafik &rarr;</a></p>
-<table cellspacing="10" style="font-size: large">
-<tbody>"""
-parameters, fencers, bouts = parse_felo_file("degen.felo")
-for fechter in calculate_felo_ratings(parameters, fencers, bouts, plot=mache_plots):
+<table cellspacing="10" style="font-size: large; width:100%%">
+<tbody>""" % convert_date(epee_bouts[-1].date[:10])
+for fechter in calculate_felo_ratings(epee_parameters, epee_fencers, epee_bouts, plot=mache_plots):
     felo_zahl = str(fechter.felo_rating)
     if fechter.fenced_points < 50:
         felo_zahl = "&asymp;&nbsp;" + felo_zahl
-    print>>ausgabedatei, u"<tr><td>%s</td><td>%s</td></tr>" % (fechter.name, felo_zahl)
+    print>>ausgabedatei, u"<tr><td>%s</td><td style='text-align:right'>%s</td></tr>" % (fechter.name, felo_zahl)
 shutil.copy2("degen.png", "/home/bronger/aktuell/fechten/Intern/")
 shutil.copy2("degen.pdf", "/home/bronger/aktuell/fechten/Intern/")
 print>>ausgabedatei, u"""</tbody>
-</table-->
+</table>
+</td-->
+</tr>
+</tbody>
+</table>
 </div>
 <p id="referenztabelle" style="max-width:30%">Als Orientierung:<!-- (Aber bitte nicht zu ernst nehmen, weil sich das
 auf internationale Wettkämpfe bezieht.)--></p>
