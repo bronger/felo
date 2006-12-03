9261e6e7b98bf389b206043c1fd796d60ce99ba8
Torsten Bronger <bronger@physik.rwth-aachen.de>
2006-12-03 12:43:49 +0000

    Ordinary update (very many changes).

--- src/felo.py
+++ src/felo.py
@@ -1,156 +1,173 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import wx, wx.grid, wx.py.editor
+import wx, wx.grid, wx.py.editor, wx.py.editwindow
 import felo_rating
-import re
+import re, os, codecs, sys, time
 
 parameters, _, fencers, bouts = felo_rating.parse_felo_file("../aachen/florett.felo")
-
-class BoutTable(wx.grid.PyGridTableBase):
-    def __init__(self):
-        wx.grid.PyGridTableBase.__init__(self)
-
-        self.odd = wx.grid.GridCellAttr()
-        self.odd.SetBackgroundColour("gray90")
-        self.odd.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD))
-        self.even = wx.grid.GridCellAttr()
-        self.even.SetBackgroundColour("white")
-        self.even.SetFont(wx.Font(10, wx.SWISS, wx.NORMAL, wx.BOLD))
-
-        self.colLabels = ["Datum", "Fechter 1", "Fechter 2", "Punkte 1", "Punkte 2",
-                          "Gefochten auf"]
-        self.dataTypes = [wx.grid.GRID_VALUE_STRING,
-                          wx.grid.GRID_VALUE_STRING,
-                          wx.grid.GRID_VALUE_STRING,
-                          wx.grid.GRID_VALUE_NUMBER + ':0,15',
-                          wx.grid.GRID_VALUE_NUMBER + ':0,15',
-                          wx.grid.GRID_VALUE_CHOICE + ':0,5,10,15']
-
-    def extract_bout_by_column(self, bout, col):
-        if col == 0:
-            return bout.date
-        elif col == 1:
-            return bout.first_fencer
-        elif col == 2:
-            return bout.second_fencer
-        elif col == 3:
-            return bout.points_first
-        elif col == 4:
-            return bout.points_second
-        elif col == 5:
-            return bout.fenced_to
-
-    def unknown_fencer_warning(self, fencer_name):
-        wx.MessageBox("Achtung: Fechter \"%s\" ist unbekannt." % fencer_name, caption="Fechter unbekannt",
-                      style=wx.OK | wx.ICON_WARNING)
-
-    def set_bout_by_column(self, bout, col, value):
-        if col == 0:
-            bout.date = value
-        elif col == 1:
-            bout.first_fencer = value
-            if not fencers.has_key(value):
-                self.unknown_fencer_warning(value)
-        elif col == 2:
-            bout.second_fencer = value
-            if not fencers.has_key(value):
-                self.unknown_fencer_warning(value)
-        elif col == 3:
-            bout.points_first = value
-        elif col == 4:
-            bout.points_second = value
-        elif col == 5:
-            bout.fenced_to = value
-
-    def GetNumberRows(self):
-        return len(bouts) + 1
-
-    def GetNumberCols(self):
-        return len(self.colLabels)
-
-    def IsEmptyCell(self, row, col):
-        try:
-            return not bouts[row]
-        except IndexError:
-            return True
-
-    def GetValue(self, row, col):
-        try:
-            return self.extract_bout_by_column(bouts[row], col)
-        except IndexError:
-            return ""
-
-    def SetValue(self, row, col, value):
-        try:
-            self.set_bout_by_column(bouts[row], col, value)
-        except IndexError:
-            # add a new row
-            if bouts:
-                year, month, day = bouts[-1].year, bouts[-1].month, bouts[-1].day
-            else:
-                year, month, day = 2006, 11, 23
-            bouts.append(felo_rating.Bout(year, month, day))
-            self.SetValue(row, col, value)
-            msg = wx.grid.GridTableMessage(self, wx.grid.GRIDTABLE_NOTIFY_ROWS_APPENDED, 1)
-            self.GetView().ProcessTableMessage(msg)
-
-    def GetAttr(self, row, col, kind):
-        attr = [self.even, self.odd][row % 2]
-        attr.IncRef()
-        return attr
-
-    def GetColLabelValue(self, col):
-        return self.colLabels[col]
-
-    def GetTypeName(self, row, col):
-        return self.dataTypes[col]
-
-    def CanGetValueAs(self, row, col, typeName):
-        colType = self.dataTypes[col].split(':')[0]
-        if typeName == colType:
-            return True
-        else:
-            return False
-
-    def CanSetValueAs(self, row, col, typeName):
-        return self.CanGetValueAs(row, col, typeName)
 
 class Editor(wx.py.editor.EditWindow):
     def __init__(self, parent):
         wx.stc.StyledTextCtrl.__init__(self, parent, wx.ID_ANY)
-        self.setStyles()
+        self.SetTabWidth(8)
+        self.setStyles(wx.py.editwindow.FACES)
+        self.SetMarginWidth(0, self.TextWidth(wx.stc.STC_STYLE_LINENUMBER, "00000"))
+        self.SetMarginWidth(1, 10)
+
+class ResultFrame(wx.Frame):
+    def __init__(self, results, *args, **keyw):
+        wx.Frame.__init__(self, None, wx.ID_ANY, size=(300, 500), title="Felo-Zahlen", *args, **keyw)
+        result_box = wx.TextCtrl(self, wx.ID_ANY, results, style=wx.TE_MULTILINE)
+        result_box.SetEditable(False)
+        
+class HTMLDialog(wx.Dialog):
+    def __init__(self, directory, *args, **keyw):
+        wx.Dialog.__init__(self, None, wx.ID_ANY, size=(300, 160), title=u"HTML-Export", *args, **keyw)
+        wx.StaticText(self, wx.ID_ANY, u"Die Web-Dateien werden in das Verzeichnis\n%s\ngeschrieben."%directory,
+                      (20, 20))
+        self.plot_switch = wx.CheckBox(self, wx.ID_ANY, u"mit Plot", (35, 80), (150, 20))
+        ok_button = wx.Button(self, wx.ID_OK, "Okay", pos=(50, 120))
+        ok_button.SetDefault()
+        cancel_button = wx.Button(self, wx.ID_CANCEL, "Abbrechen", pos=(150, 120))
 
 class Frame(wx.Frame):
     def __init__(self, *args, **keyw):
-        wx.Frame.__init__(self, None, wx.ID_ANY, size=(700, 200), title="Felo-Zahlen", *args, **keyw)
+        wx.Frame.__init__(self, None, wx.ID_ANY, size=(700, 700), title="Felo", *args, **keyw)
         menu_bar = wx.MenuBar()
 
         menu_file = wx.Menu()
-        menu_file.Append(wx.ID_ANY, u"Ö&ffnen")
-        menu_file.Append(wx.ID_ANY, u"&Speichern")
-        menu_file.Append(wx.ID_ANY, u"Speichern &unter")
+        open = menu_file.Append(wx.ID_ANY, u"Ö&ffnen")
+        self.Bind(wx.EVT_MENU, self.OnOpen, open)
+        save = menu_file.Append(wx.ID_ANY, u"&Speichern")
+        self.Bind(wx.EVT_MENU, self.OnSave, save)
+        save_as = menu_file.Append(wx.ID_ANY, u"Speichern &unter")
+        self.Bind(wx.EVT_MENU, self.OnSaveAs, save_as)
         menu_file.AppendSeparator()
         exit = menu_file.Append(wx.ID_ANY, u"&Beenden")
         self.Bind(wx.EVT_MENU, self.OnExit, exit)
         menu_bar.Append(menu_file, u"&Datei")
 
         menu_numbers = wx.Menu()
-        menu_numbers.Append(wx.ID_ANY, u"&Fechter bearbeiten")
-        menu_numbers.Append(wx.ID_ANY, u"&Parameter einsehen/ändern")
-        menu_numbers.AppendSeparator()
-        menu_numbers.Append(wx.ID_ANY, u"Felo-Zahlen &berechnen")
+        calculate_felo_numbers = menu_numbers.Append(wx.ID_ANY, u"Felo-Zahlen &berechnen")
+        self.Bind(wx.EVT_MENU, self.OnCalculateFeloNumbers, calculate_felo_numbers)
+        generate_html = menu_numbers.Append(wx.ID_ANY, u"&HTML erzeugen")
+        self.Bind(wx.EVT_MENU, self.OnGenerateHTML, generate_html)
         menu_bar.Append(menu_numbers, u"&Bearbeiten")
 
         self.SetMenuBar(menu_bar)
 
-        self.editor = Editor(self)
-#         self.table = BoutTable()
-#         self.grid = wx.grid.Grid(self)
-#         self.grid.SetTable(self.table, True, selmode=wx.grid.Grid.SelectRows)
-
+        self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
+        self.felo_filename = self.editor = None
+        if len(sys.argv) > 1:
+            self.open_felo_file(sys.argv[1])
+    def OnModify(self, event):
+        self.felo_file_modified = True
+    def AssureSave(self):
+        if self.editor and self.felo_file_modified:
+            answer = wx.MessageBox(u"Die Datei \"%s\" wurde verändert.  Abspeichern?" % self.felo_filename,
+                                   u"Datei modifiziert", wx.YES_NO | wx.CANCEL | wx.YES_DEFAULT |
+                                   wx.ICON_QUESTION, self)
+            if answer == wx.YES:
+                self.editor.SaveFile(self.felo_filename)
+                self.felo_file_modified = False
+            elif answer == wx.CANCEL:
+                return wx.CANCEL
+    def open_felo_file(self, felo_filename):
+        self.felo_filename = os.path.abspath(felo_filename)
+        if not self.editor:
+            self.editor = Editor(self)
+            self.editor.Bind(wx.stc.EVT_STC_MODIFIED, self.OnModify)
+        self.editor.LoadFile(self.felo_filename)
+        self.felo_file_modified = False
+        self.SetTitle(u"Felo – "+os.path.split(self.felo_filename)[1])
+        os.chdir(os.path.dirname(self.felo_filename))
+    def OnOpen(self, event):
+        if self.AssureSave == wx.CANCEL:
+            return
+        wildcard = u"Felo-Datei (*.felo)|*.felo|" \
+            "Alle Dateien (*.*)|*.*"
+        dialog = wx.FileDialog(None, u"Felo-Datei wählen", os.getcwd(),
+                               "", wildcard, wx.OPEN | wx.FILE_MUST_EXIST)
+        if dialog.ShowModal() == wx.ID_OK:
+            self.open_felo_file(dialog.GetPath())
+            dialog.Destroy()
     def OnExit(self, event):
         self.Close()
+    def OnCloseWindow(self, event):
+        if self.AssureSave() == wx.CANCEL:
+            return
+        self.Destroy()
+    def save_felo_file(self):
+        if self.felo_filename:
+            self.editor.SaveFile(self.felo_filename)
+            self.felo_file_modified = False
+            return True
+        return False
+    def OnSave(self, event):
+        self.save_felo_file()
+    def OnSaveAs(self, event):
+        if self.editor:
+            wildcard = u"Felo-Datei (*.felo)|*.felo|" \
+                "Alle Dateien (*.*)|*.*"
+            dialog = wx.FileDialog(None, u"Felo-Datei wählen", os.getcwd(),
+                                   "", wildcard, wx.SAVE | wx.OVERWRITE_PROMPT)
+            if dialog.ShowModal() == wx.ID_OK:
+                self.felo_filename = dialog.GetPath()
+                dialog.Destroy()
+                self.save_felo_file()
+    def OnCalculateFeloNumbers(self, event):
+        if not self.save_felo_file():
+            wx.MessageBox(u"Bitte öffne erst eine Felo-Datei." ,
+                          u"Hinweis", wx.OK | wx.ICON_INFORMATION, self)
+            return
+        parameters, _, fencers, bouts = felo_rating.parse_felo_file(self.felo_filename)
+        fencerlist = felo_rating.calculate_felo_ratings(parameters, fencers, bouts)
+        results = u""
+        for fencer in fencerlist:
+            results += fencer.name + u"\t\t" + unicode(fencer.felo_rating) + u"\n"
+        result_frame = ResultFrame(results)
+        result_frame.Show()
+    def OnGenerateHTML(self, event):
+        if not self.save_felo_file():
+            wx.MessageBox(u"Bitte öffne erst eine Felo-Datei." ,
+                          u"Hinweis", wx.OK | wx.ICON_INFORMATION, self)
+            return
+        if self.AssureSave() == wx.CANCEL:
+            return
+        parameters, _, fencers, bouts = felo_rating.parse_felo_file(self.felo_filename)
+        bouts.sort()
+        last_date = time.strftime(u"%d.&nbsp;%B&nbsp;%Y", time.strptime(bouts[-1].date[:10], "%Y/%m/%d"))
+        base_filename = parameters[u"Gruppenname"].lower()
+        html_dialog = HTMLDialog(parameters[u"Ausgabeverzeichnis"])
+        result = html_dialog.ShowModal()
+        make_plot = html_dialog.plot_switch.GetValue()
+        html_dialog.Destroy()
+        if result != wx.ID_OK:
+            return
+        html_file = codecs.open(os.path.join(parameters[u"Ausgabeverzeichnis"], base_filename+".html"),
+                                "w", "utf-8")
+        print>>html_file, u"""<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head><title>%(title)s</title><meta http-equiv="Content-Style-Type" content="text/css" /><style type="text/css">
+/*<![CDATA[*/
+@import "felo.css";
+/*]]>*/
+</style></head><body>\n\n<h1>%(title)s</h1>\n<h2>%(date)s</h2>\n\n<table><tbody>""" % \
+            {"title": u"Felo-Zahlen "+parameters[u"Gruppenname"], "date": u"vom "+last_date}
+        fencerlist = felo_rating.calculate_felo_ratings(parameters, fencers, bouts, plot=make_plot)
+        for fencer in fencerlist:
+            print>>html_file, u"<tr><td class='name'>%s</td><td class='felo-rating'>%d</td></tr>" % \
+                (fencer.name, fencer.felo_rating)
+        print>>html_file, u"</tbody></table>"
+        if make_plot:
+            print>>html_file, u"<img class='felo-plot' src='%s.png' alt='%s' />" % \
+                (base_filename, u"Felo-Zahlen-Plot für "+parameters[u"Gruppenname"])
+            print>>html_file, u"<p class='printable-notice'>Auch in einer <a href='%s.pdf'>" \
+                "ausdruckbaren Version</a>.<p>" % base_filename
+        print>>html_file, u"</body></html>"
+        html_file.close()
 
 class App(wx.App):
     def OnInit(self):
