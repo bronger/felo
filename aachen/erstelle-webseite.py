e4863410c20a1d6bf827aaa84a6b2be4140f00aa
Torsten Bronger <bronger@physik.rwth-aachen.de>
2006-12-04 09:32:53 +0000

    Gültiges XHTML 1.0 strict draus gemacht.

--- aachen/erstelle-webseite.py
+++ aachen/erstelle-webseite.py
@@ -30,40 +30,43 @@ epee_parameters, _, epee_fencers, epee_bouts = parse_felo_file(codecs.open("dege
 foil_bouts.sort()
 epee_bouts.sort()
 
-print>>ausgabedatei, u"""<html>
+print>>ausgabedatei, u"""<?xml version="1.0" encoding="iso-8859-1"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <title>Felo-Zahlen</title>
-<meta name="author" content="Torsten">
-<meta name="generator" content="Torstens Fechter-Zahl-Generator">
-<link rel=stylesheet type="text/css" href="../Style.css">
+<meta name="author" content="Torsten" />
+<meta name="generator" content="Torstens Felo-Zahl-Generator" />
+<link rel="stylesheet" type="text/css" href="../Style.css" />
 </head>
 <body>
-<div align="center">
+<div style="text-align:center">
 <h1>Felo-Zahlen</h1>
 
 <p style="font-size: large">Die <a href="felo-faq.html">Felo-FAQ</a> &ndash;
                             Fragen und Antworten</p>
 
-<table cellspacing="50px" style="background-color: #00a0FF">
+<table cellspacing="50px" style="background-color: #00a0FF" width="100%%">
 <tbody>
 <tr style="vertical-align:top">
-<td style="text-align:center">
+<td>&nbsp;</td>
+<td style="text-align:center;width:19em">
 <h2 id="florett">Florett</h2>
 <p style="font-size: medium;margin-top:-1ex">vom %s</p>
 <p style="font-size:large"><a href="felo-plots.html">Zur Grafik &rarr;</a></p>
-<p><table cellspacing="10" style="font-size: large; width:100%%">
+<table cellspacing="10" style="font-size: large; width:100%%">
 <tbody>""" % convert_date(foil_bouts[-1].date[:10])
 # write_felo_file("florett_.felo", foil_parameters, foil_fencers, foil_bouts)
 for fechter in calculate_felo_ratings(foil_parameters, foil_fencers, foil_bouts, plot=mache_plots):
     felo_zahl = str(fechter.felo_rating)
     if fechter.total_weighting < 7:
         felo_zahl = "&asymp;&nbsp;" + felo_zahl
-    print>>ausgabedatei, u"<tr><td>%s</td><td style='text-align:right'>%s</td></tr>" % (fechter.name, felo_zahl)
+    print>>ausgabedatei, u"<tr><td style='text-align:left'>%s</td><td style='text-align:right'>%s</td></tr>" % (fechter.name, felo_zahl)
 shutil.copy2("florett.png", "/home/bronger/aktuell/fechten/Statistics/")
 shutil.copy2("florett.pdf", "/home/bronger/aktuell/fechten/Statistics/")
 print>>ausgabedatei, u"""</tbody>
-</table></p>
-</td><td style="text-align:center">
+</table>
+</td><td style="text-align:center;width:19em">
 <h2 id="degen">Degen</h2>
 <p style="font-size: medium;margin-top:-1ex">vom %s</p>
 <p style="font-size:large"><a href="felo-plots.html#degen">Zur Grafik &rarr;</a></p>
@@ -73,12 +76,13 @@ for fechter in calculate_felo_ratings(epee_parameters, epee_fencers, epee_bouts,
     felo_zahl = str(fechter.felo_rating)
     if fechter.total_weighting < 7:
         felo_zahl = "&asymp;&nbsp;" + felo_zahl
-    print>>ausgabedatei, u"<tr><td>%s</td><td style='text-align:right'>%s</td></tr>" % (fechter.name, felo_zahl)
+    print>>ausgabedatei, u"<tr><td style='text-align:left'>%s</td><td style='text-align:right'>%s</td></tr>" % (fechter.name, felo_zahl)
 shutil.copy2("degen.png", "/home/bronger/aktuell/fechten/Statistics/")
 shutil.copy2("degen.pdf", "/home/bronger/aktuell/fechten/Statistics/")
 print>>ausgabedatei, u"""</tbody>
 </table>
-</td-->
+</td>
+<td>&nbsp;</td>
 </tr>
 </tbody>
 </table>
