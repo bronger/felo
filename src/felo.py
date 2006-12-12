4548268d198857d5f947bc5463fec5b662524314
Torsten Bronger <bronger@physik.rwth-aachen.de>
2006-12-12 12:48:10 +0000

    Added encoding information to HTML output.

--- src/felo.py
+++ src/felo.py
@@ -419,7 +419,8 @@ class Frame(wx.Frame):
         print>>html_file, u"""<?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head><title>%(title)s</title><meta http-equiv="Content-Style-Type" content="text/css" /><style type="text/css">
+<head><title>%(title)s</title><meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="Content-Style-Type" content="text/css" /><style type="text/css">
 /*<![CDATA[*/
 @import "felo.css";
 /*]]>*/
